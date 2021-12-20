7a3130735c3122cefe71a21bca74807de38f5930
askeko <askeklitgaard@gmail.com>
2021-12-20 22:52:20 +0100

    applied barpadding patch - needs merging with status2d, systray and maybe other bar patches to work properly

--- config.h
+++ config.h
@@ -21,6 +21,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 0;        /* vertical padding for statusbar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "JetBrains Mono:style:medium:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";

947cf003f35c63e0b35bb9d50aa15ee1dcb7f85e
askeko <askeklitgaard@gmail.com>
2021-12-24 13:21:18 +0100

    Resize bar
    Made the bar a little larger for cosmetic purposes. This patch is also
    to test out my git workflow, rebasing rather than merge committing.

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static const Bool toptab            = False;    /* False means bottom tab bar */
 
 static const unsigned int colorfultag = 1; /* 0 means use SchemeSel for selected tag */
 static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 8;        /* vertical padding for statusbar */
+static const int vertpadbar         = 10;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "JetBrains Mono:style:medium:size=11" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";

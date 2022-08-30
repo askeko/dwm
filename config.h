27441de7ec891064c6747a2ba2ca03e92094c45f
askeko <askeklitgaard@gmail.com>
2022-08-30 18:25:19 +0200

    Add icon

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰅨", "󰆍", "󰈹", "󰭻", "󰤽", "T" };
+static const char *tags[] = { "󰅨", "󰆍", "󰈹", "󰭻", "󰤽", "󰈙" };
 
 /* default layout per tags */
 /* The first element is for all-tag view, following i-th element corresponds to */

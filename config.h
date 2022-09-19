41122600c141e01a3ef952e7f1b08c879c3ee82b
askeko <askeklitgaard@gmail.com>
2022-09-19 15:05:23 +0200

    Add new workspace

--- config.h
+++ config.h
@@ -67,7 +67,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰅨", "󰆍", "󰈹", "󰭻", "󰤽", "󰈙" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
 
 /* default layout per tags */
 /* The first element is for all-tag view, following i-th element corresponds to */
